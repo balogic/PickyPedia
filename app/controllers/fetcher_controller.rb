6840fa57da8605af3ae8ef37c19f8eb9d90d5e17
iamBalaji <rbalajives@gmail.com>
2015-06-05 19:25:00 +0530

    final code

--- app/controllers/fetcher_controller.rb
+++ app/controllers/fetcher_controller.rb
@@ -1,4 +1,11 @@
 class FetcherController < ApplicationController
 	def index
+		client = MediawikiApi::Client.new "http://en.wikipedia.org/w/api.php"
+		@images = []
+		ror = client.query( :list => "allimages", :ailimit => 5, :aifrom => params[:search] ).data["allimages"]
+		ror.each do |r|
+			d = r["url"]
+			@images.push(d)
 		end
+	end
 end
